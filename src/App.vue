a65016093c61ef31d7e0a02b39ea438a19274c27
rparejoc <34284762+rparejoc@users.noreply.github.com>
2026-04-23 22:12:16 +0200

    feat: migrate navigation to vue router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
 <script setup>
-import { computed, onMounted, onUnmounted, ref } from 'vue'
-import AuthPage from './pages/AuthPage.vue'
-import FamilyPage from './pages/FamilyPage.vue'
-
-const currentPath = ref(window.location.pathname)
-
-const syncPath = () => {
-  currentPath.value = window.location.pathname
-}
-
-onMounted(() => {
-  window.addEventListener('popstate', syncPath)
-})
-
-onUnmounted(() => {
-  window.removeEventListener('popstate', syncPath)
-})
-
-const showFamilyPage = computed(() => currentPath.value === '/dashboard')
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <FamilyPage v-if="showFamilyPage" />
-  <AuthPage v-else />
+  <RouterView />
 </template>
